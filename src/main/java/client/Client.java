3bdb72354a4ea5905ffec0a6c2e2310080d5d031
DaronAdams <daron.cole@icloud.com>
2023-11-25 18:06:19 -0600

    Cleaned up the rest of the help menu commands

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -16,8 +16,6 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Client {
     private BufferedReader in;
@@ -73,15 +71,18 @@ public class Client {
         });
     }
 
+    /*
+    * Adds a command to the list of suggestions when a user types "/"
+     */
     private void showCommandSuggestions(DocumentEvent e) {
         try {
             String text = textField.getText(0, e.getOffset() + e.getLength());
             if (text.endsWith("/")) {
                 JPopupMenu suggestionsMenu = new JPopupMenu();
-                suggestionsMenu.add(createSuggestionItem("/list", "Lists all online users"));
-                suggestionsMenu.add(createSuggestionItem("/exit", "Exits the chat"));
-                suggestionsMenu.add(createSuggestionItem("/clear", "Clears all messages from the chat history"));
-                suggestionsMenu.add(createSuggestionItem("/msg", "Sends a private message. Usage: /msg <username> <message>"));
+                suggestionsMenu.add(createSuggestionItem("/list"));
+                suggestionsMenu.add(createSuggestionItem("/exit"));
+                suggestionsMenu.add(createSuggestionItem("/clear"));
+                suggestionsMenu.add(createSuggestionItem("/msg"));
                 suggestionsMenu.show(textField, 0, textField.getHeight());
             }
         } catch (BadLocationException ex) {
@@ -89,7 +90,10 @@ public class Client {
         }
     }
 
-    private JMenuItem createSuggestionItem(String command, String description) {
+    /*
+    * Helper method to add a suggestion item to the JPane menu
+     */
+    private JMenuItem createSuggestionItem(String command) {
         JMenuItem item = new JMenuItem(command);
         item.addActionListener(e -> {
             textField.setText(command + " ");
@@ -97,6 +101,7 @@ public class Client {
         });
         return item;
     }
+
     /*
     * UI prompt for user to enter server address
      */
@@ -196,7 +201,7 @@ public class Client {
 
     public static void main(String[] args) throws IOException {
         try {
-            // Setting up the theme
+            // Setting the theme
             UIManager.setLookAndFeel(new FlatDarculaLaf());
         } catch (Exception ex) {
             ex.printStackTrace();
